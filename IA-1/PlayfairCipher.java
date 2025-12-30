c4183d2231953e6e1c4633b9fba9955b1c4bb7a6
Yogesh Patil <yp901981@gmail.com>
2025-12-30 20:04:21 +0530

    Update PlayfairCipher.java

--- IA-1/PlayfairCipher.java
+++ IA-1/PlayfairCipher.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 public class PlayFair {
@@ -10,13 +11,14 @@ public class PlayFair {
 
         for (int i = 0; i < key.length(); i++) {
             char ch = key.charAt(i);
-            if (ch < 'A' || ch > 'Z') continue;  
+            if (ch < 'A' || ch > 'Z') continue;
             int idx = ch - 'A';
             if (!used[idx]) {
                 used[idx] = true;
                 sb.append(ch);
             }
         }
+
         for (char ch = 'A'; ch <= 'Z'; ch++) {
             int idx = ch - 'A';
             if (!used[idx]) {
@@ -24,22 +26,23 @@ public class PlayFair {
                 sb.append(ch);
             }
         }
+
         char[][] matrix = new char[5][5];
         int k = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
                 matrix[i][j] = sb.charAt(k++);
-            }
-        }
+
         return matrix;
     }
 
     static String prepareText(String text) {
         text = text.toUpperCase().replace('J', 'I');
         StringBuilder sb = new StringBuilder();
-        for (char ch : text.toCharArray()) {
-            if (ch >= 'A' && ch <= 'Z') sb.append(ch);
-        }
+
+        for (char ch : text.toCharArray())
+            if (ch >= 'A' && ch <= 'Z')
+                sb.append(ch);
 
         StringBuilder prepared = new StringBuilder();
         for (int i = 0; i < sb.length(); i++) {
@@ -48,35 +51,36 @@ public class PlayFair {
             if (i + 1 < sb.length() && sb.charAt(i + 1) == ch)
                 prepared.append('X');
         }
+
         if (prepared.length() % 2 == 1)
             prepared.append('X');
+
         return prepared.toString();
     }
 
     static int[] pos(char[][] matrix, char ch) {
-        for (int r = 0; r < 5; r++) {
-            for (int c = 0; c < 5; c++) {
-                if (matrix[r][c] == ch)
-                    return new int[]{r, c};
-            }
-        }
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
+                if (matrix[i][j] == ch)
+                    return new int[]{i, j};
         return null;
     }
 
     static String processPair(char[][] matrix, char a, char b, boolean encrypt) {
         int[] pa = pos(matrix, a);
         int[] pb = pos(matrix, b);
+
         int ra = pa[0], ca = pa[1];
         int rb = pb[0], cb = pb[1];
 
         if (ra == rb) {
-            int s = encrypt ? 1 : 4;
-            ca = (ca + s) % 5;
-            cb = (cb + s) % 5;
+            int shift = encrypt ? 1 : 4;
+            ca = (ca + shift) % 5;
+            cb = (cb + shift) % 5;
         } else if (ca == cb) {
-            int s = encrypt ? 1 : 4;
-            ra = (ra + s) % 5;
-            rb = (rb + s) % 5;
+            int shift = encrypt ? 1 : 4;
+            ra = (ra + shift) % 5;
+            rb = (rb + shift) % 5;
         } else {
             int temp = ca;
             ca = cb;
@@ -86,47 +90,46 @@ public class PlayFair {
         return "" + matrix[ra][ca] + matrix[rb][cb];
     }
 
-    static String encrypt(char[][] matrix, String prepared) {
+    static String encrypt(char[][] matrix, String text) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < prepared.length(); i += 2) {
-            char a = prepared.charAt(i);
-            char b = prepared.charAt(i + 1);
-            sb.append(processPair(matrix, a, b, true));
-        }
+        for (int i = 0; i < text.length(); i += 2)
+            sb.append(processPair(matrix, text.charAt(i), text.charAt(i + 1), true));
         return sb.toString();
     }
 
-    static String decrypt(char[][] matrix, String prepared) {
+    static String decrypt(char[][] matrix, String text) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < prepared.length(); i += 2) {
-            char a = prepared.charAt(i);
-            char b = prepared.charAt(i + 1);
-            sb.append(processPair(matrix, a, b, false));
-        }
+        for (int i = 0; i < text.length(); i += 2)
+            sb.append(processPair(matrix, text.charAt(i), text.charAt(i + 1), false));
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter text to encrypt:");
-        String text = sc.nextLine();
+    public static void main(String[] args) throws Exception {
 
-        String key = "MONARCHY";
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        PrintWriter pw = new PrintWriter(new FileWriter("output.txt"));
+
+        String plaintext = br.readLine();
+        String key = br.readLine();
+
         char[][] matrix = generateMatrix(key);
-        String prepared = prepareText(text);
+        String prepared = prepareText(plaintext);
         String cipher = encrypt(matrix, prepared);
         String decrypted = decrypt(matrix, cipher);
 
-        System.out.println("\nMatrix:");
+        pw.println("Playfair Cipher Matrix:");
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+            for (int j = 0; j < 5; j++)
+                pw.print(matrix[i][j] + " ");
+            pw.println();
         }
 
-        System.out.println("\nPrepared Text: " + prepared);
-        System.out.println("Encrypted Text: " + cipher);
-        System.out.println("Decrypted Text: " + decrypted);
+        pw.println("\nPlain Text: " + plaintext);
+        pw.println("Prepared Text: " + prepared);
+        pw.println("Encrypted Text: " + cipher);
+        pw.println("Decrypted Text: " + decrypted);
+
+        br.close();
+        pw.close();
     }
 }
